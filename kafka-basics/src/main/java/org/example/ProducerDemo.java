5b45177eeabe18dea3f2f90d6689476336d78de6
Michael <mibergdev@gmail.com>
2024-02-01 15:57:27 +0200

    first topic works

--- kafka-basics/src/main/java/org/example/ProducerDemo.java
+++ kafka-basics/src/main/java/org/example/ProducerDemo.java
@@ -12,15 +12,12 @@ public class ProducerDemo {
     private static final Logger log =
             LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
     public static void main(String[] args) {
-        log.info("Hello world");
-
         Properties properties = new Properties();
-
         // connect to kafka on localhost
         //properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
 
         // connect to cloud kafka cluster
-        properties.setProperty("bootstrap.servers", "Spkc-gy65n.europe-west12.gcp.confluent.cloud:9092");
+        properties.setProperty("bootstrap.servers", "pkc-gy65n.europe-west12.gcp.confluent.cloud:9092");
         properties.setProperty("security.protocol", "SASL_SSL");
         properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username='YPH7PX6RYT6OBX7G' password='TspUbYo+6lgUFlvRS4HAeinXRmB1pdbln5wwbH1Pf2gxE13aj9ZJGqvjUTgX4T83';");
         properties.setProperty("sasl.mechanism", "PLAIN");
